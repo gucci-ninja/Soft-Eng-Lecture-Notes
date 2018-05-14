49756f581674c03a7f4fa89730d43439c3eec9d3
Suhavi Sandhu <sandhs11@mcmaster.ca>
2018-05-14 00:03:38 -0400

    setup summer folder

--- TableOfContents.rb
+++ TableOfContents.rb
@@ -49,7 +49,7 @@ end
 
 
 filenames = ["3B/2B03/", "3B/3A04/", "3B/3DX4/", "3B/3I03/", "3B/3S03/",
-"3B/4A03/", "3B/1BA3/", "3A/3DB3/", "3A/3BB4/"]
+"3B/4A03/", "3B/1BA3/", "3A/3DB3/", "3A/3BB4/", "Summer/Fast.ai/", "Summer/Algorithms and Data Structures/"]
 
 filenames.each do |fname|
     puts fname
